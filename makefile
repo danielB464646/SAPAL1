ad988ba233526dc5ad1d80fbcb54d449545e60c0
Daniel Blanga <blangad@post.bgu.ac.il>
2024-12-01 09:29:05 +0000

    selection policy 1

--- makefile
+++ makefile
@@ -1,13 +1,16 @@
 # Please implement your Makefile rules and targets below.
 # Customize this file to define how to build your project.
+
+# Compiler flags
+CXXFLAGS = -g -O0 -Iinclude
+
 all: clean compile run
 
 compile:
-	g++ -Iinclude -o main src/main.cpp
+	g++ $(CXXFLAGS) -o main src/main.cpp
 
 run:
 	./main
 
 clean:
 	rm -f main
-
--- src/SelectionPolicy.cpp
+++ src/SelectionPolicy.cpp
@@ -1,7 +1,9 @@
 #include "SelectionPolicy.h"
+#include <algorithm> 
+#include <stdexcept>
 
 
-//constructor
+//---------------------------NAIVESELECTION---------------------------------------------
 NaiveSelection::NaiveSelection(){
     lastSelectedIndex=0;
 };
@@ -22,7 +24,7 @@ const FacilityType& NaiveSelection::selectFacility(const vector<FacilityType>& f
 
 // Method to return the string representation of NaiveSelection
 const string NaiveSelection::toString() const {
-    return "NaiveSelection";  // A simple string for demonstration, can be expanded
+    return "NaiveSelection lastselectedindex="+this->lastSelectedIndex;  // A simple string for demonstration, can be expanded
 }
 
 // Method to clone the NaiveSelection object
@@ -30,23 +32,112 @@ NaiveSelection* NaiveSelection::clone() const {
     return new NaiveSelection(*this);  // Return a new instance with the same state
 }
 
-//-------------
+NaiveSelection::~NaiveSelection(){}//has no dynamic memory to release
 
-BalancedSelection::BalancedSelection(int LifeQualityScoreVal, int EconomyScoreVal, int EnvironmentScoreVal)
+//-------------BALANCEDSELECTION----------------------------------------------------------------------------
+
+BalancedSelection::BalancedSelection(int LifeQualityScoreVal, int EconomyScoreVal, int EnvironmentScoreVal) : 
+LifeQualityScore(LifeQualityScoreVal),EconomyScore(EconomyScoreVal), EnvironmentScore(EnvironmentScoreVal)
 {
-    LifeQualityScore = LifeQualityScoreVal;
-    EconomyScore = EconomyScoreVal;
-    EnvironmentScore = EnvironmentScoreVal;
+
 }
 
 const FacilityType& BalancedSelection::selectFacility(const vector<FacilityType>& facilitiesOptions)
 {
-    int maxbalance =9999;
-    int maxbalanceindex=0;
+
+    if (facilitiesOptions.empty()) {
+        throw std::invalid_argument("Facilities options cannot be empty.");
+    }
+    
+    int mostbalance =9999;
+    const FacilityType* toret= &facilitiesOptions[0];
+    //int maxbalanceindex=0;
 
     for (size_t i = 0; i < facilitiesOptions.size(); ++i) {
-        const FacilityType& facility = facilitiesOptions[i];
-        facility.getEconomyScore();  
+        const FacilityType& currfacility = facilitiesOptions[i];
+
+        int potentialecon = this->EconomyScore +  currfacility.getEconomyScore();
+        int potentiallife = this->LifeQualityScore + currfacility.getLifeQualityScore();
+        int potentialenviro = this->EnvironmentScore - currfacility.getEnvironmentScore();
+
+
+        int maxgap = std::abs(std::max({potentialecon, potentiallife, potentialenviro}) - std::min({potentialecon, potentiallife, potentialenviro}));
+
+        if(maxgap<mostbalance)
+        {mostbalance = maxgap;
+            toret = &currfacility; 
+        }
+    }
+    return *toret; // because the function signature is to return reference it will return reference.
+
+
+}
+
+// Method to return the string representation of NaiveSelection
+const string BalancedSelection::toString() const {
+    return "NaiveSelection obj lifequality: "+this->LifeQualityScore +"  enviro score: "+ this->EnvironmentScore+" economyscore "+this->EconomyScore;  // A simple string for demonstration, can be expanded
+}
+
+// Method to clone the NaiveSelection object
+BalancedSelection* BalancedSelection::clone() const {
+    return new BalancedSelection(*this);  // Return a new instance with the same state
+}
+
+BalancedSelection::~BalancedSelection(){}//no dynamic memory to release
+
+//------------ECONOMYSELECTION-----------------------------------------------------------
+EconomySelection::EconomySelection(){
+    lastSelectedIndex =0;
+}
+
+const FacilityType& EconomySelection::selectFacility(const vector<FacilityType>& facilitiesOptions)
+{
+    int temp;
+    while(lastSelectedIndex<facilitiesOptions.size())
+    {
+        if(facilitiesOptions[lastSelectedIndex].getCategory()==FacilityCategory::ECONOMY)
+        {
+            temp = lastSelectedIndex;
+            lastSelectedIndex+=1;
+            return facilitiesOptions[temp];
+
+        }
+        lastSelectedIndex+=1;
+        if(lastSelectedIndex==facilitiesOptions.size())
+        {lastSelectedIndex=0;}
+    }
+}
+
+
+const FacilityType& EconomySelection::selectFacility(const vector<FacilityType>& facilitiesOptions)
+{
+    // Check if there is any ECONOMY facility in the list
+    bool economyFound = false;
+    for (const auto& facility : facilitiesOptions) {
+        if (facility.getCategory() == FacilityCategory::ECONOMY) {
+            economyFound = true;
+            break;
+        }
     }
 
+    // If no ECONOMY facility exists, throw an error
+    if (!economyFound) {
+        throw std::runtime_error("No ECONOMY facility found.");
+    }
+
+    // Proceed with the main loop to select ECONOMY facilities
+    while (true) {
+        if (facilitiesOptions[lastSelectedIndex].getCategory() == FacilityCategory::ECONOMY) {
+            // Return the facility of type ECONOMY
+            const FacilityType& selectedFacility = facilitiesOptions[lastSelectedIndex];
+
+            // Move to the next facility in the list (loop back to the beginning if needed)
+            lastSelectedIndex = (lastSelectedIndex + 1) % facilitiesOptions.size();
+
+            return selectedFacility;
+        }
+
+        // If the current facility is not ECONOMY, just move to the next one
+        lastSelectedIndex = (lastSelectedIndex + 1) % facilitiesOptions.size();
+    }
 }
