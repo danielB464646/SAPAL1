c1e9b9188d8dec553b416bd18fea689e32327089
stav <stavrone@post.bgu.ac.ilgit config user.email stavrone@post.bgu.ac.ilgit config user.email stavrone@post.bgu.ac.il>
2024-12-01 09:44:22 +0000

    kishkashta

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 Simulation* backup = nullptr;
 
 int main(int argc, char** argv){
-    cout<<"AAAAAAAAAAAAA"<<std::endl;
+    cout<<"kishkashta"<<std::endl;
     /*
     if(argc!=2){
         cout << "usage: simulation <config_path>" << endl;
